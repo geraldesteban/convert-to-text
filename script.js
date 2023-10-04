faae71c782343fb3b4bd905f88207d86fb58c30e
geraldesteban <wakin.esteban@gmail.com>
2023-10-04 21:16:28 +0800

    initial commit

--- script.js
+++ script.js
@@ -1,160 +1,152 @@
 "use strict";
 
 /* Change UI */
-const changeImage = document.querySelector(".change-image");
-const changeVideo = document.querySelector(".change-video");
-const convertVideo = document.querySelector(".convert-video");
-const convertImage = document.querySelector(".convert-image");
+$(document).ready(function () {
+  const changeImage = $(".change-image");
+  const changeVideo = $(".change-video");
+  const convertVideo = $(".convert-video");
+  const convertImage = $(".convert-image");
 
-convertVideo.style.display = "none";
-convertImage.style.display = "block";
+  convertVideo.hide();
+  convertImage.show();
 
-changeVideo.addEventListener("click", () => {
-  convertVideo.style.display = "block";
-  convertImage.style.display = "none";
-});
+  changeVideo.click(function () {
+    convertVideo.show();
+    convertImage.hide();
+  });
 
-changeImage.addEventListener("click", () => {
-  convertVideo.style.display = "none";
-  convertImage.style.display = "block";
+  changeImage.click(function () {
+    convertVideo.hide();
+    convertImage.show();
+  });
 });
 
 /* Image to Text Converter Functionality */
-const imageUpload = document.getElementById("image-upload");
-const imageConvertedText = document.getElementById("image-converted-text");
+$(document).ready(function () {
+  const imageUpload = $("#image-upload");
+  const imageConvertedText = $("#image-converted-text");
 
-const doImageOCR = async () => {
-  const file = imageUpload.files[0];
-  if (file) {
-    try {
-      const objectURL = URL.createObjectURL(file);
-      const img = new Image();
+  const doImageOCR = async () => {
+    const file = imageUpload[0].files[0]; // Use [0] to access the first element in the jQuery object.
+    if (file) {
+      try {
+        const objectURL = URL.createObjectURL(file);
+        const img = new Image();
 
-      // Set the width and height of the image to match the video dimensions.
-      img.width = 640;
-      img.height = 360;
+        // Set the width and height of the image to match the video dimensions.
+        img.width = 640;
+        img.height = 360;
 
-      img.src = objectURL;
-      img.crossOrigin = "anonymous";
+        img.src = objectURL;
+        img.crossOrigin = "anonymous";
 
-      // Remove the previously displayed image and clear the converted text.
-      while (imageConvertedText.firstChild) {
-        imageConvertedText.removeChild(imageConvertedText.firstChild);
-      }
+        // Remove the previously displayed image and clear the converted text.
+        imageConvertedText.empty();
 
-      img.addEventListener("load", async () => {
-        try {
-          // Use Tesseract.js to perform OCR on the loaded image.
-          const {
-            data: { text },
-          } = await Tesseract.recognize(img);
+        img.addEventListener("load", async () => {
+          try {
+            // Use Tesseract.js to perform OCR on the loaded image.
+            const {
+              data: { text },
+            } = await Tesseract.recognize(img);
 
-          // Display the converted text in the "image-converted-text" element.
-          imageConvertedText.innerHTML = `<b>${text}</b>`;
-        } catch (error) {
-          console.error(error);
+            // Display the converted text in the "image-converted-text" element.
+            imageConvertedText.html(`<b>${text}</b>`);
+          } catch (error) {
+            console.error(error);
+          }
+        });
+
+        // Remove the previously displayed image.
+        const previousImage = $("#image-root img");
+        if (previousImage.length) {
+          previousImage.remove();
         }
-      });
 
-      // Remove the previously displayed image.
-      const previousImage = document
-        .getElementById("image-root")
-        .querySelector("img");
-      if (previousImage) {
-        previousImage.remove();
+        $("#image-root").append(img);
+      } catch (error) {
+        console.error(error);
       }
-
-      document.getElementById("image-root").appendChild(img);
-    } catch (error) {
-      console.error(error);
     }
-  }
-};
+  };
 
-imageUpload.addEventListener("change", doImageOCR);
+  imageUpload.on("change", doImageOCR);
+});
 
 /* Video to Text Converter Functionality */
-const { createWorker, createScheduler } = Tesseract;
-const scheduler = createScheduler();
-const messages = document.getElementById("video-messages");
-const videoUpload = document.getElementById("video-upload");
-const convertedText = document.getElementById("video-converted-text");
-let timerId = null;
-let videoText = ""; // Store the text from the video.
-let currentVideoElement = null; // Track the currently displayed video element.
+$(document).ready(function () {
+  const scheduler = Tesseract.createScheduler();
+  const messages = $("#video-messages");
+  const videoUpload = $("#video-upload");
+  const convertedText = $("#video-converted-text");
+  let timerId = null;
+  let videoText = "";
+  let currentVideoElement = null;
 
-const addMessage = (m, bold) => {
-  let msg = `<p>${m}</p>`;
-  if (bold) {
-    msg = `<p class="bold">${m}</p>`;
-  }
-  messages.innerHTML += msg; // Append the newly recognized text
-  messages.scrollTop = messages.scrollHeight;
-};
+  const addMessage = (m, bold) => {
+    let msg = `<p>${m}</p>`;
+    if (bold) {
+      msg = `<p class="bold">${m}</p>`;
+    }
+    messages.append(msg);
+    messages.scrollTop(messages[0].scrollHeight);
+  };
 
-const doOCR = async () => {
-  // Check if there is a currently displayed video element and remove it.
-  if (currentVideoElement) {
-    currentVideoElement.remove();
-  }
+  const doOCR = async () => {
+    if (currentVideoElement) {
+      currentVideoElement.remove();
+    }
 
-  const video = document.createElement("video");
-  video.width = 640;
-  video.height = 360;
-  video.crossOrigin = "anonymous";
-  video.autoplay = true; // Add autoplay attribute.
+    const video = $("<video>")
+      .attr("width", 640)
+      .attr("height", 360)
+      .attr("crossOrigin", "anonymous")
+      .attr("autoplay", true);
 
-  const c = document.createElement("canvas");
-  c.width = video.width;
-  c.height = video.height;
+    const canvas = $("<canvas>").attr("width", 640).attr("height", 360)[0];
 
-  const file = videoUpload.files[0];
-  if (file) {
-    const objectURL = URL.createObjectURL(file);
-    video.src = objectURL;
-    video.controls = true;
-    currentVideoElement = video; // Set the current video element.
+    const file = videoUpload[0].files[0];
 
-    video.addEventListener("play", () => {
-      timerId = setInterval(async () => {
-        c.getContext("2d").drawImage(video, 0, 0, video.width, video.height);
-        const {
-          data: { text },
-        } = await scheduler.addJob("recognize", c);
+    if (file) {
+      const objectURL = URL.createObjectURL(file);
+      video.attr("src", objectURL).attr("controls", true);
+      currentVideoElement = video[0];
 
-        // Filter out unwanted characters (e.g., non-alphanumeric characters)
-        const filteredText = text.replace(/[^a-zA-Z0-9\s]/g, "");
+      video.on("play", () => {
+        timerId = setInterval(async () => {
+          canvas
+            .getContext("2d")
+            .drawImage(video[0], 0, 0, video[0].width, video[0].height);
+          const {
+            data: { text },
+          } = await scheduler.addJob("recognize", canvas);
 
-        // Store the text from the video.
-        videoText += filteredText;
+          const filteredText = text.replace(/[^a-zA-Z0-9\s]/g, "");
+          videoText += filteredText;
+          addMessage(filteredText);
+        }, 1000);
+      });
 
-        // Append the filtered text without removing the previous content.
-        addMessage(filteredText);
-      }, 1000);
-    });
+      video.on("pause", () => {
+        clearInterval(timerId);
+        convertedText.text(videoText);
+        addMessage("Video processing complete.", true);
+      });
 
-    video.addEventListener("pause", () => {
-      clearInterval(timerId);
+      $("#video-root").append(video);
+      addMessage("Video uploaded. You can now play the video.", true);
+    }
+  };
 
-      // Set the text from the video in the converted-text div.
-      convertedText.textContent = videoText;
+  videoUpload.on("change", doOCR);
 
-      addMessage("Video processing complete.", true); // Display this message in bold.
-    });
-
-    document.getElementById("video-root").appendChild(video); // Use the correct container ID
-    addMessage("Video uploaded. You can now play the video.", true); // Display this message in bold.
-  }
-};
-
-videoUpload.addEventListener("change", doOCR);
-
-(async () => {
-  for (let i = 0; i < 4; i++) {
-    const worker = createWorker();
-    await worker.load();
-    await worker.loadLanguage("eng");
-    await worker.initialize("eng");
-    scheduler.addWorker(worker);
-  }
-})();
+  (async () => {
+    for (let i = 0; i < 4; i++) {
+      const worker = Tesseract.createWorker();
+      await worker.load();
+      await worker.loadLanguage("eng");
+      await worker.initialize("eng");
+      scheduler.addWorker(worker);
+    }
+  })();
+});
